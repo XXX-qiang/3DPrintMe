36a787ed5a204ff95a1eeb877d05e6ae502d8e85
Kieran Hannigan <overyonderstudios@gmail.com>
2016-09-09 17:47:20 +0800

    Finished commenting and added more boilerplate. (#3)

--- src/pkg3dprintme/FXMLDocumentController.java
+++ src/pkg3dprintme/FXMLDocumentController.java
@@ -1,21 +1,34 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2016 Kieran
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package pkg3dprintme;
 
 import java.net.URL;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 
 /**
- * The FXMLDocumentController class contains the business logic of the 
+ * The FXMLDocumentController class contains all of the front-end logic of the
  * application. Components and methods specified in the FXML GUI layout 
  * are injected into the controller using JavaFX FXML injection points.
  * 
@@ -43,15 +56,22 @@ public class FXMLDocumentController implements Initializable {
      */
     
     /**
-     *
-     * @param event 
+     * The captureButtonAction is an injected method that will be called
+     * whenever the capture button on the interface is pressed.
+     * @param event the internal event which triggers this handler.
      */
     @FXML
     private void captureButtonAction(ActionEvent event) {
         ZonedDateTime zdt;
         zdt = ZonedDateTime.now();
-        boolean success = networkController.capture("Kieran", zdt, "C:\\");
-        resultsLabel.setText(success ? "Success!" : "Failure!");
+        try {
+            networkController.capture("Kieran", zdt, "C:\\");
+            resultsLabel.setText("Success!");
+        } catch (Exception e) {
+            // TODO: Display detailed error messages
+            resultsLabel.setText("Failure!");
+        }
+        
     }
 
     /* ---------------------------
@@ -60,7 +80,7 @@ public class FXMLDocumentController implements Initializable {
      */
     
     /**
-     * 
+     * The initialize function performs all of the required instantiation.
      * @param url
      * @param rb 
      */
@@ -69,30 +89,200 @@ public class FXMLDocumentController implements Initializable {
         networkController = new NetworkController();
     }    
 
+    /** 
+     * The NetworkController class contains all of the back-end logic of the
+     * application. An instance of this class is created by the application
+     * controller, and that controller will call public methods in this class
+     * as part of handling interface events.
+     */
     private static class NetworkController {
 
-        HashMap<String, Integer> hostTable;
-        
+        /**
+         * The constructor for this class, which performs all of the required
+         * instantiation.
+         */
         public NetworkController() {
         }
         
-        public boolean capture(String name, ZonedDateTime date, String path) {
-            hostTable = statusQuery();
-            return (checkMap(hostTable));
+        /**
+         * The capture function captures images from all of the ImageServers
+         * that are available, and them stores them to the file system. 
+         * The parameters to this function are provided by the
+         * interface controller which retrieves them from the layout and
+         * performs sanitization before sending them here.
+         * 
+         * @param name the name of the client/job.
+         * @param date the datetime of the job.
+         * @param path the path to save the job.
+         */
+        public void capture(String name, ZonedDateTime date, String path) throws Exception {
+            ArrayList<Image> images;
+            
+            try {
+                images = getImages();
+                storeImages(images, path);
+            } catch(Exception e) {
+                // TODO: Implement retries, error handling, and rethrowing
+                throw e;
+            }
+            
         }
         
-        private HashMap<String, Integer> statusQuery() {
-            HashMap<String, Integer> hosts;
-            hosts = new HashMap<>();
-            return hosts;
+        /**
+         * The getImages function captures images from all of the detected
+         * ImageServers. This function is called by the capture function,
+         * but can also be called by the interface controller for previewing.
+         * 
+         * @return an ArrayList of images.
+         */
+        public ArrayList<Image> getImages() throws Exception {
+            HashMap<String, String> hostTable;
+            ArrayList<Image> imageSet;
+            imageSet = null;
+            
+            try {
+                hostTable = statusQuery();
+                if (!checkMap(hostTable))  {
+                    throw new Exception();
+                }
+                imageSet = shoot(hostTable);
+            } catch(Exception e) {
+                // TODO: Implement retries, error handling, and rethrowing
+                throw e;
+            }
+            
+            return imageSet;
         }
         
-        private boolean checkMap(HashMap<String, Integer> hostTable) {
-            return hostTable.entrySet().stream().noneMatch((entry) -> (!checkEntry(entry)));
+        /**
+         * This function queries all available ImageServers using the UDP
+         * protocol broadcast address.
+         * 
+         * @return a map from String to String of all the hosts that were 
+         *         detected, where the key is the name of the host and the 
+         *         value is that host's IP address.
+         */
+        private HashMap<String, String> statusQuery() {
+            HashMap<String, String> detectedHosts;
+            detectedHosts = new HashMap<>();
+            
+            try {
+                // TODO: Use UDP to scan for hosts and build the detectedHosts 
+                //       table.
+            } catch (Exception e) {
+                // TODO: Implement retries, error handling, and rethrowing
+                throw e;
+            }
+            return detectedHosts;
+        }
+        
+        /**
+         * This function checks a host table for errors - it accomplishes
+         * this by iterating through the table and checking each entry
+         * individually.
+         * 
+         * @param hostTable the host table to be checked.
+         * @return a boolean representing success (true) or failure (false).
+         */
+        private boolean checkMap(HashMap<String, String> hostTable) {
+            return (!hostTable.isEmpty() && hostTable.entrySet().stream().noneMatch((entry) -> (!checkEntry(entry))));
         }
 
-        private boolean checkEntry(HashMap.Entry<String, Integer> entry) {
+        /**
+         * This function checks a host entry for errors - errors include
+         * such things as invalid host names, invalid IP addresses, clashes,
+         * and mismatches.
+         * 
+         * @param entry the host entry to be checked.
+         * @return a boolean representing success (true) or failure (false).
+         */
+        private boolean checkEntry(HashMap.Entry<String, String> entry) {
+            // TODO: implement host entry checking
             return false;
+        }
+
+        /**
+         * The shoot function coordinates requests to the ImageServers. It
+         * first builds a table of delay adjustments, then issues a command
+         * for the ImageServers to capture.
+         * 
+         * @param hostTable the table of ImageServers
+         * @return the images captured.
+         */
+        private ArrayList<Image> shoot(HashMap<String, String> hostTable) {
+            ArrayList<Image> capturedImages;
+            capturedImages = new ArrayList<>();
+            
+            HashMap<String, Integer> syncTable;
+            
+            try {
+                syncTable = sync(hostTable);
+                capturedImages = snap(hostTable, syncTable);
+            } catch (Exception e) {
+                // TODO: Implement retries, error handling, and rethrowing
+                throw e;
+            }
+            
+            return capturedImages;
+        }
+
+        /**
+         * The storeImages function saves images to the local file system.
+         * 
+         * @param imageSet the images to be saved.
+         * @param savePath the path where the images should be saved.
+         */
+        private void storeImages(ArrayList<Image> imageSet, String savePath) {
+            try {
+                // TODO: implement image storing
+            } catch (Exception e) {
+                // TODO: Implement retries, error handling, and rethrowing
+                throw e;
+            }
+        }
+
+        /**
+         * The sync function builds up a table of delays based on the time
+         * that it takes to reach a group of hosts.
+         * 
+         * @param hostTable the hosts to be reached.
+         * @return 
+         */
+        private HashMap<String, Integer> sync(HashMap<String, String> hostTable) {
+            HashMap<String, Integer> observedDelays;
+            observedDelays = null;
+            
+            try {
+                // TODO: Build sync table using custom TCP handshake
+            } catch (Exception e) {
+                // TODO: Implement retries, error handling, and rethrowing
+                throw e;
+            }
+            
+            return observedDelays;
+        }
+
+        /**
+         * The snap function commands a group of ImageServers to capture and
+         * return their payload at a given unified time.
+         * 
+         * @param syncTable the table of hosts and their addresses.
+         * @param syncTable the table of host delays.
+         * @return 
+         */
+        private ArrayList<Image> snap(HashMap<String, String> hostTable, 
+                                        HashMap<String, Integer> syncTable) {
+            ArrayList<Image> receivedImages;
+            receivedImages = null;
+            
+            try {
+                // TODO: Capture images from ImageServers
+            } catch (Exception e) {
+                // TODO: Implement retries, error handling, and rethrowing
+                throw e;
+            }
+            
+            return receivedImages;
         }
 
     }
